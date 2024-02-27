5ee3b2e05f419abde85311d6ad918d8bb6556f29
AidenAC <aidenc@umich.edu>
2024-02-27 17:13:12 -0500

    added button to link to soundcloud songs

--- FrontEnd/updatemyusb_front/lib/main.dart
+++ FrontEnd/updatemyusb_front/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,60 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'UpdateMyUSB',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: theme.colorScheme.primary,
+        title: Text(
+          'Update My USB',
+          style: style,
+          ),
+      ),
+      body: const Center(
+        child: SongLink(),
+      ),
+    );
+  }
+}
+
+class SongLink extends StatelessWidget {
+  const SongLink({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displaySmall!.copyWith(
+      color: theme.colorScheme.onSurface
+    );
+
+    return Link(
+      target: LinkTarget.self,
+      uri: Uri.parse('https://soundcloud.com/sub49records/fefo-electric-siren-saturday-night-fever'),
+      builder: (context, followLink) => ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: theme.colorScheme.surface),
+        onPressed: followLink,
+        child: Text(
+          'Saturday Night Fever',
+          style: style,
         ),
       ),
     );
