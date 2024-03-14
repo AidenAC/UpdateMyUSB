5b95a1da28f567b5b51cb807831afab8e4aafeb6
AidenAC <aidenc@umich.edu>
2024-03-14 17:59:51 -0400

    created login page

--- FrontEnd/updatemyusb_front/lib/user.dart
+++ FrontEnd/updatemyusb_front/lib/user.dart
@@ -2,13 +2,22 @@
 
 import 'package:flutter/material.dart';
 
-class UserPage extends StatelessWidget {
+class UserPage extends StatefulWidget {
   const UserPage({super.key});
 
   @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  var userIndex = 0;
+  late Widget userAction;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
+    switch (userIndex) {
+      case 0:
+        userAction = Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(
@@ -16,6 +25,9 @@ class UserPage extends StatelessWidget {
             child: ElevatedButton(
               onPressed: () {
                 print('login');
+                setState(() {
+                  userIndex = 1;
+                });
               },
               child: const Text('Login'),
             ),
@@ -30,7 +42,70 @@ class UserPage extends StatelessWidget {
             ),
           ),
         ],
-      ),
+      );
+      case 1:
+        userAction = Column(
+          children: [
+            Row(
+              children: [
+                BackButton(
+                  onPressed: () {
+                    setState(() {
+                      userIndex = 0;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 12.0,
+                bottom: 12.0,
+                left: 50.0,
+                right: 50.0,
+              ),
+              child: Form(
+                child: Column (
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 12.0),
+                      child: ElevatedButton(
+                        onPressed: () {print('logged in');},
+                        child: const Text('Login'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Dont have an account?'),
+                TextButton(
+                  onPressed: () {print('create an account');},
+                  child: const Text('Create Account'),
+                ),
+              ],
+            ),
+          ],
+        );
+      default:
+        throw UnimplementedError();
+    }
+
+    return Center(
+      child: userAction,
     );
   }
 }
