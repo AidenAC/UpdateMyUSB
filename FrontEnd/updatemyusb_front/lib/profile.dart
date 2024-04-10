166eed6bcc6e96a9422adbe2c4eba601187efb26
AidenAC <aidenc@umich.edu>
2024-04-10 16:08:52 -0400

    add provider to user profile

--- FrontEnd/updatemyusb_front/lib/profile.dart
+++ FrontEnd/updatemyusb_front/lib/profile.dart
@@ -15,9 +15,20 @@ class ProfilePage extends StatelessWidget {
         Text('Email: ${user.email}'),
         Text('Location: ${user.location}'),
 
+        //DJ Profile
         Visibility(
           visible: user.isDJ,
           child: Text('DJ Name: ${dj.djname}'),
+        ),
+
+        //Provider Profile
+        Visibility(
+          visible: user.isProvider,
+          child: Text('Provider Name: ${provider.providername}'),
+        ),
+        Visibility(
+          visible: user.isProvider,
+          child: Text('Provider Type: ${provider.getType()}'),
         ),
       ],
     );
